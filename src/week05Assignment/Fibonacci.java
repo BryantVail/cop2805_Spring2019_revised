a9adeeb04ce07c399dd36b37998442b1eaf2e704
Bryant Vail <Bryanttv@outlook.com>
2019-02-11 23:50:43 -0500

    update

--- src/week05Assignment/Fibonacci.java
+++ src/week05Assignment/Fibonacci.java
@@ -20,4 +20,32 @@ public class Fibonacci {
 			return fib(index -1)+ fib(index -2);
 		}
 	}
+	
+	public long fibEfficient(long n) {
+		
+		long f0 =0;
+		long f1 =1;
+		long f2 =1;
+		
+		if(n == 0) {
+			return f0;
+		}else if(n ==1) {
+			return f1;
+		}else if(n ==2) {
+			return f2;
+		}
+		
+		for(int i=3; i<=n; i++) {
+			f0 = f1;
+			f1 = f2;
+			f2 = f0 + f1;
+		}
+		
+		return f2;
+	}
 }
+
+
+
+
+
