ab0704b4c8a5e8cde4e840ec87ed92d3f0419f1a
Bryant Vail <Bryanttv@outlook.com>
2019-02-25 02:50:00 -0500

    update

--- Week07/src/week07/QuizPractice.java
+++ Week07/src/week07/QuizPractice.java
@@ -1,10 +1,18 @@
 package week07;
 
+import java.util.ArrayList;
+import java.lang.Object;
+
 public class QuizPractice {
 	
 	public static void main(String[] args) {
 		
-		
+		ArrayList<E> list1 = new ArrayList();
+		StopWatch sw = new StopWatch();
+		for(int i = 0; i< 100000; i++) {
+			list1.add(i);
+			
+		}
 	}
 
 }
