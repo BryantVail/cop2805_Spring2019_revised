e570ee77a391937b441ec0a31e4ae9acd0b4adbd
Bryant Vail <Bryanttv@outlook.com>
2019-02-25 23:43:20 -0500

    Update QuizPractice.java

--- Week07/src/week07/QuizPractice.java
+++ Week07/src/week07/QuizPractice.java
@@ -2,17 +2,20 @@ package week07;
 
 import java.util.ArrayList;
 import java.lang.Object;
+import java.time.LocalDateTime;
+
 
 public class QuizPractice {
 	
 	public static void main(String[] args) {
 		
-		ArrayList<E> list1 = new ArrayList();
-		StopWatch sw = new StopWatch();
+		ArrayList<Integer> list1 = new ArrayList();
+		LocalDateTime before = LocalDateTime.now();
 		for(int i = 0; i< 100000; i++) {
 			list1.add(i);
 			
 		}
+		LocalDateTime difference = LocalDateTime. LocalDateTime.now() - before
 	}
 
 }
